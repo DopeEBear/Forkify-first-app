30e936d6461fc01ef10246c64551b33c4870e930
dopeebear <johnathon_brown11@yahoo.com>
2021-02-28 12:24:52 -0800

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -124,6 +124,9 @@ const controlAddRecipe = async function (newRecipe) {
     addRecipeView.renderError(err.message);
   }
 };
+const newFeature = function () {
+  console.log('Welcome to the application');
+};
 
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
@@ -133,6 +136,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log('Welcome!');
+  newFeature();
 };
 init();
